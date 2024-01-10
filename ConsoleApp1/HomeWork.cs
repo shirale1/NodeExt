fba779e54c0002d3fea5e90d152f979c90b6b478
User <User@LAB3-3>
2024-01-10 11:14:07 +0200

    ..

--- ConsoleApp1/HomeWork.cs
+++ ConsoleApp1/HomeWork.cs
@@ -30,5 +30,55 @@ namespace ConsoleApp1
                 return counter + 1;
             return counter;
         }
+        public static int WhichBigger(Node<int> lst1,Node<int> lst2) 
+            //כתוב פעולה חיצונית המקבלת שתי רשימות  כל רשימה מייצגת מספר שלם גדול מאוד.
+            // מספר שלם גדול מאוד יוצג ברשימה באופן הבא: כל חולייה ברשימה מכילה ספרה של המספר
+            //הספרה האחרונה ברשימה היא ספרת האחדות
+            //על הפעולה להחזיר 1 אם המספר המיוצג ברשימה הראשונה גדול יותר, 2 אם המספר השני גדול יותר
+            //ו0 אם המספרים שווים
+        {
+            int count1 = 0;
+            int count2 = 0;
+            Node<int> head1 = lst1;
+            Node<int> head2 = lst2;
+            while(lst1!= null)
+            {
+                count1++;
+                lst1 = lst1.GetNext();
+            }
+            lst1 = head1;
+            while (lst2 != null)
+            {
+                count2++;
+                lst2 = lst2.GetNext();
+            }
+            lst2 = head2;
+            if (count1 > count2)
+            {
+                return 1;
+            }
+            if(count1 < count2)
+            {
+                return 2;
+            }
+           
+            
+                while (lst1!= null)
+                {
+                    if (lst1.GetValue() > lst2.GetValue())
+                    {
+                        return 1;
+                    }
+                    else if (lst1.GetValue() < lst2.GetValue())
+                    {
+                        return 2;
+                    }
+                    lst1 = lst1.GetNext();
+                    lst2 = lst2.GetNext();
+                }
+                return 0;
+             
+            
+        }
     }
 }
